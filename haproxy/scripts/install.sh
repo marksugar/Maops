d4fc0cb4692978de21d65c11e0d19dff3cf0f6be
Mark <usertzc@gmail.com>
2018-12-27 21:16:54 +0800

    Create install.sh

---
+++ haproxy/scripts/install.sh
@@ -0,0 +1,18 @@
+PRDIR=/usr/local/haproxy
+curl -Lk https://www.haproxy.org/download/1.9/src/haproxy-1.9.0.tar.gz |tar xz -C /usr/local/
+cd /usr/local/haproxy-1.9.0 && make TARGET=linux2628 ARCH=x86_64 PREFIX=$PRDIR
+make install PREFIX=$PRDIR
+
+mkdir /etc/haproxy/ -p
+useradd haproxy -s /sbin/nologin -M
+cp /usr/local/haproxy/sbin/haproxy /usr/sbin/
+cp /usr/local/haproxy-1.9.0/examples/haproxy.init /etc/init.d/haproxy
+
+
+chmod +x /etc/init.d/haproxy
+chkconfig haproxy on
+chkconfig --add haproxy
+
+curl -Lk https://raw.githubusercontent.com/marksugar/docker-haproxy1.6.5/master/haproxy.cfg -o /etc/haproxy/haproxy.cfg
+systemctl start haproxy
+haproxy -v
