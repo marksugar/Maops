86a7dbaa2daf149a2844e4144730ae522b02e932
Mark <usertzc@gmail.com>
2019-06-29 20:20:40 +0800

    chenage if else

--- redis-cli/install.sh
+++ redis-cli/install.sh
@@ -1,4 +1,18 @@
-git clone http://github.com/antirez/redis.git && mv redis /usr/local/
-cd /usr/local/redis && git checkout 3.0 
-make redis-cli 
-ln -s /usr/local/redis/src/redis-cli /bin/redis-cli 
+#!/bin/bash
+install_redis_cli(){
+  git clone http://github.com/antirez/redis.git && mv redis /usr/local/
+  cd /usr/local/redis && git checkout 3.0 
+  make redis-cli
+  ln -s /usr/local/redis/src/redis-cli /bin/redis-cli 
+}
+if type redis-cli > /dev/null 2>&1; then
+    echo "redis-cli exists"
+    exit 1;
+else
+    if type git >/dev/null 2>&1; then
+      install_redis_cli
+    else
+      echo 'no exists git'
+      echo "git packets not fount"
+    fi
+fi
